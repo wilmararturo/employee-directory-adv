dd4ce4ad93c625521a69cf374720cbef85bfbe66
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-07-18 00:01:17 -0400

    implement passportjs #1

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+require("./config/passport");
 
-const routes = require('./controllers');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const passport = require("passport");
+const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(
+  session({
+    secret: "hammer apple toothpaste",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // turn on routes
 app.use(routes);
