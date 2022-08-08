38e5ac9b049de72bc1827f231440eda09845b853
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-08-07 23:24:48 -0400

    initial commit #4

---
+++ utils/crypto.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const crypto = require("crypto");
+
+const hash = (data) => {
+  return crypto.createHash("sha256").update(data).digest("hex");
+};
+
+module.exports = { hash };
