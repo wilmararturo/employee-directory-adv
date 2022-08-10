4217e0c3ea0f7136d97bfa96fcd6c8b3deca2b40
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2022-08-09 23:19:43 -0400

    add redis caching #5

---
+++ config/redis.js
@@ -0,0 +1,20 @@
+const redis = require("redis");
+require("dotenv").config();
+
+let redisClient;
+
+async function connectRedis() {
+  redisClient = redis.createClient({
+    port: process.env.REDPORT,
+  });
+  await redisClient.connect();
+}
+connectRedis();
+
+redisClient.on("error", (err) =>
+  console.log(`Couldn\'t establish a connection with redis: ${err}`)
+);
+
+redisClient.on("connect", () => console.log(`Connected to redis successfully`));
+
+module.exports = redisClient;
--- config/session.js
+++ config/session.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const session = require("express-session");
 const crypto = require("crypto");
+const RedisStore = require("connect-redis")(session);
+const redisClient = require("./redis");
 
 const serverSession = session({
   genid: function (req) {
     return crypto.randomBytes(32).toString("hex");
   },
+  store: new RedisStore({ client: redisClient }),
   resave: false,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET,
